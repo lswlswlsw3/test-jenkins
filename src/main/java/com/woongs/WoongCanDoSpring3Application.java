b1c82ffbcebc75417fca461d8256ac3f1e8dc6a9
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-11-08 16:33:05 +0900

    spring gradle 설정파일 커밋

---
+++ src/main/java/com/woongs/WoongCanDoSpring3Application.java
@@ -0,0 +1,12 @@
+package com.woongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoongCanDoSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoongCanDoSpring3Application.class, args);
+	}
+}
